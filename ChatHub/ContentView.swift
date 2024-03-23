bc97ca3530b0362fee0d5fd2e3bce74b52d669a2
Tej <gurtej1992@gmail.com>
2024-03-23 02:57:29 -0400

    Working LoginView UI

--- ChatHub/ContentView.swift
+++ ChatHub/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLogin = false
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView{
+                VStack{
+                    Picker(selection: $isLogin, label: Text("Picker")){
+                        Text("Login").tag(true)
+                        Text("Register").tag(false)
+                    }.pickerStyle(.segmented)
+                    if !isLogin{
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text(Image(systemName: "person.fill")).font(.system(size: 64))
+                        })
+                    }
+                    
+                    
+                    Group{
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(.white)
+                    
+                    
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        HStack{
+                            Spacer()
+                            Text(isLogin ? "Login" : "Register").font(.system(size: 18).bold()).foregroundStyle(.white).padding(13)
+                            Spacer()
+                        }.background(.blue)
+                        
+                    })
+                    
+                }.padding()
+                
+            }.navigationTitle(isLogin ? "Login" : "Register")
+                .background(Color(uiColor: .systemGray5))
         }
-        .padding()
     }
 }
 
