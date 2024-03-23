8b6e35465379206b6153e9b9e79e674db537279b
Tej <gurtej1992@gmail.com>
2024-03-23 02:22:41 -0400

    Initial Commit

---
+++ ChatHub/ChatHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatHubApp.swift
+//  ChatHub
+//
+//  Created by Gurtej Singh on 2024-03-23.
+//
+
+import SwiftUI
+
+@main
+struct ChatHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatHub
+//
+//  Created by Gurtej Singh on 2024-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
